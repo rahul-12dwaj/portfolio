de22cd1040a2c8daddb87019528438f4e32b8ee9
Rahul Bhardwaj <152688275+rahul-12dwaj@users.noreply.github.com>
2025-07-25 09:27:09 +0530

    Update Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -9,7 +9,7 @@ const projects = [
     title: "Crime Report Management & FIR Tracking System",
     description:
       "A full-stack web application with FIR and Crime report Managing system.",
-    image: "/CrimeManagement.png",
+    image: "/crime-management.png",
     link: "https://crimemanagement.onrender.com/",
     techStack: "MERN Stack",
   },
